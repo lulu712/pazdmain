55b0e59f12e5f7d4cc6050e1fd37cd86eace0afa
ㄧlulu <benz9779@gmail.com>
2026-01-23 14:15:06 +0800

    正確版菜單修改1

---
+++ src/components/paneHead.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="panel-heading">
+        <div class="panel-lead">菜單管理</div>
+        <p class="description">菜單規則通常對應一個控制器的方法,同時菜單欄位數據也從規則中獲取</p>
+    </div>
+</template>
+
+<script setup></script>
+
+<style lang="less" scoped >
+
+.panel-heading {
+    padding: 15px;
+    background: #e8edf0;
+    border-color: #e8edf0;
+    position: relative;
+.panel-lead {
+      font-size: 14px;
+.title {
+        font-weight: bold;
+        font-style: normal;
+      }
+.description {
+        margin-top: 5px;
+      }
+    }
+  }
+
+</style>
